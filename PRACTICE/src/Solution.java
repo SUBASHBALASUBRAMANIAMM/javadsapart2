12c87276272814d25e09dbafb45e7e36a247f907
SUBASH B K <subashbalasubramaniamm@gmail.com>
2023-02-01 14:28:17 +0530

    update

--- PRACTICE/src/Solution.java
+++ PRACTICE/src/Solution.java
@@ -1,39 +1,39 @@
-//import java.util.Arrays;
-//public class Solution {
-//    public static void main(String[] args) {
-//        String[] word = {"cat","bt","hat","tree"};
-//        String chars = "atach";
-//            int i=0;
-//
-//        for(int k = 0;k<word.length;k++){
-//            if(found(word[k],chars)){
-//                i= i + word[k].length();
-//                System.out.println(i);
-//            }}
-//
-//
-//    }
-//
-//    public static boolean  found(String word,String chars){
-//        char[] word1=word.toCharArray();
-//        char[] chars1=chars.toCharArray();
-//        int count =0;
-//
-//        for(int i=0;i<word1.length;i++){
-//            for(int j = 0;j<chars1.length;j++){
-//                if(word1[i]==chars1[j]){
-//                    chars1[j]=0;
-//                   // System.out.println(Arrays.toString(chars1));
-//                    count++;
-//                    break;
-//                }
-//            }
-//        }
-//        if(count==word.length()){
-//
-//            return true;}
-//       // System.out.println(count);
-//       return false;
-//
-//    }
-//}
+import java.util.Arrays;
+public class Solution {
+    public static void main(String[] args) {
+        String[] word = {"cat","bt","hat","tree"};
+        String chars = "atach";
+            int i=0;
+
+        for(int k = 0;k<word.length;k++){
+            if(found(word[k],chars)){
+                i= i + word[k].length();
+                System.out.println(i);
+            }}
+
+
+    }
+
+    public static boolean  found(String word,String chars){
+        char[] word1=word.toCharArray();
+        char[] chars1=chars.toCharArray();
+        int count =0;
+
+        for(int i=0;i<word1.length;i++){
+            for(int j = 0;j<chars1.length;j++){
+                if(word1[i]==chars1[j]){
+                    chars1[j]=0;
+                   // System.out.println(Arrays.toString(chars1));
+                    count++;
+                    break;
+                }
+            }
+        }
+        if(count==word.length()){
+
+            return true;}
+       // System.out.println(count);
+       return false;
+
+    }
+}
